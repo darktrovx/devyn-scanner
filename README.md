88a9b8c88dfecbb47cb8256897890619f8ab09b1
Devyn <mage1d@yahoo.com>
2022-08-10 11:37:43 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Item
+```
+['scanner'] 			 		 = {['name'] = 'scanner', 						['label'] = 'Scanner', 		['weight'] = 5000, 		['type'] = 'item', 		['image'] = 'radioscanner.png', 		['unique'] = false, 		['useable'] = true, 	['shouldClose'] = true,	   ['combinable'] = nil,   ['description'] = 'Used to scan for radio channels'},
+
+```
---
+++ server/Main.lua
@@ -0,0 +1,27 @@
+local QBCore = exports["qb-core"]:GetCoreObject()
+
+QBCore.Functions.CreateUseableItem("scanner", function(source, item)
+    TriggerClientEvent("radio:scan", source)
+end)
+
+QBCore.Functions.CreateCallback("radio:RadiosInRange", function(source, cb, coords)
+
+    local players = QBCore.Functions.GetQBPlayers()
+    local channels = {}
+    for k, v in pairs(players) do
+        local pcoords = GetEntityCoords(GetPlayerPed(v.PlayerData.source))
+        local distance = #(coords - pcoords)
+        if distance <= Config.ScannerRange then
+            local state = Player(v.PlayerData.source).state
+            local channel = state["radioChannel"]
+            print(channel)
+            if channel ~= 0 then
+                channels[channel] = {
+                    min = (channel - math.random(1, Config.GuessRangeMin)), 
+                    max = (channel - math.random(1, Config.GuessRangeMax))
+                }
+            end
+        end
+    end
+    cb(channels)
+end)
